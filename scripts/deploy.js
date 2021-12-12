aac80449b4c89fb01b4c61241559fa64c60e7743
Rakesh5777 <rakesh.k@gofrugal.com>
2021-12-13 01:21:08 +0530

    Rakesh|fix| updated ropsten address

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -28,5 +28,6 @@ runMainDeploy();
 
 // 0x3e95C7892FfE071b04F0ECeb9289C920D033062d twitter 3 - basic (1.0) address in kovan network
 // 0x3f7c905bFd2b40b28Bc32741a860D1169400B6d1 twitter 3 - with msg storage(2.0) address in kovan network
+// 0xa3cb30F8d494CC3695582996ABC58b704f8e84bE twitter 3 - with spam protection(3.0) address in ropsten network
 // 0xE7874A6145b66988F06e8befB360E5Aa945cd970 my metamask address
 
